aee8318f3255d57637e0ebd82eed2b3738c5eed1
sushie <24848927+pikulet@users.noreply.github.com>
2026-04-05 22:26:27 +0800

    feat: stamp trading (#2647)
    1. support this book on desktop mode
    <img width="688" height="337" alt="image"
    src="https://github.com/user-attachments/assets/c91e2c43-1924-4fb5-8796-e1773b1aa992"
    />
    
    - fixed total stamp count should not include event
    
    2. support mods (admin) able to directly award stamp to people. idk,
    might be to reward icon designers or for event prizes without having to
    "win a game as XXX".
    <img width="312" height="119" alt="image"
    src="https://github.com/user-attachments/assets/8cbdd075-cef1-4cf3-99f7-ab905a8efe7c"
    />
    
    3. support stamp trading
    <img width="584" height="398" alt="image"
    src="https://github.com/user-attachments/assets/eb435fc7-74e0-406a-9fb0-b74f14baabd5"
    />
    <img width="329" height="448" alt="image"
    src="https://github.com/user-attachments/assets/c92618da-8e81-4bef-a873-b3884a53ee4e"
    />

--- data/constants.js
+++ data/constants.js
@@ -36,6 +36,7 @@ if (process.env.NODE_ENV.includes("development")) {
     fileAppeal: 5 * 60 * 100,
     staffApplication: 24 * 60 * 60 * 100,
     poke: 5 * 100,
+    tradeStamp: 2 * 100,
   };
 } else {
   rates = {
@@ -55,6 +56,7 @@ if (process.env.NODE_ENV.includes("development")) {
     fileAppeal: 5 * 60 * 1000,
     staffApplication: 24 * 60 * 60 * 1000,
     poke: 5 * 1000,
+    tradeStamp: 2 * 1000,
   };
 }
 
@@ -483,6 +485,7 @@ module.exports = {
     manageCompetitive: true,
     adjustMinGames: true,
     awardTrophy: true,
+    awardStamp: true,
     deleteStrategy: true,
     deleteFanart: true,
     seeModPanel: true,
@@ -546,6 +549,7 @@ module.exports = {
         "createPoll",
         "manageCompetitive",
         "awardTrophy",
+        "awardStamp",
         "deleteStrategy",
         "deleteFanart",
         "seeModPanel",
